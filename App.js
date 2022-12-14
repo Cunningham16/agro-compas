1b74f264de41097b169e82e7bc766cf9ea994a39
Ivan Karshakevich <83078128+Cunningham16@users.noreply.github.com>
2022-12-14 14:17:13 +0000

    adding assets

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'rubik': require('./assets/fonts/Rubik.ttf'),
+    'open-sans': require('./assets/fonts/OpenSans.ttf'),
+    'cousine-regular': require('./assets/fonts/Cousine-Regular.ttf'),
+    'cousine-bold': require('./assets/fonts/Cousine-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text style={styles.text}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
@@ -18,7 +40,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    fontFamily: 'rubik',
+    fontWeight: '400',
     fontSize: 40,
-    fontWeight: 'bold',
   }
 });
---
+++ constants.js
@@ -0,0 +1,29 @@
+export const COLORS = {
+	primary: '#01942C',
+	secondary: '#F3F3F3',
+
+	white: '#FFFFFF',
+	black: '#000000',
+	gray: '#111111',
+    dragLineActive: '#1BA3FE',
+    dragLineUnactive: '#C4C4C4'
+}
+
+export const SIZES = {
+	// global sizes
+	base: 8,
+	font: 14,
+	radius: 20,
+	padding: 24,
+
+	// font sizes
+	h1: 30,
+	h2: 22,
+	h3: 16,
+	h4: 14,
+	body1: 30,
+	body2: 22,
+	body3: 16,
+	body4: 14,
+	body5: 12,
+}
